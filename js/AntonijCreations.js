8227c4b1ba32ba1d80694a6407945fe91cad7c09
Gloria Aeterna <antonij.bozickogmail.com>
2023-05-09 23:15:06 +0200

    Setting the foundations

---
+++ js/AntonijCreations.js
@@ -0,0 +1,27 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//Hamburger menu button
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+}
